07415ecef854aa65550b4899a8253a277270ad5c
Katerina <ganina.katerina2915@gmail.com>
2018-11-25 22:19:18 +0200

    style for product list

--- src/modules/product-html.js
+++ src/modules/product-html.js
@@ -8,9 +8,10 @@ let _makeHtml = ({
 }) => {
 	let $product = $(`<div class="card col-xs-12 col-sm-4 col-md-3" data-product-id="${id}">`);
 	$product.append($(`<img src="${image_url}" alt="${name, price, special_price}" class="img-fluid product-image">`));
-	$product.append($(`<span class="product-title">`).text(name, price, special_price));
+	$product.append($(`<span class="product-title">`).text(name));
 	$product.append($(`<span class="product-price">`).text(price));
 	$product.append($(`<span class="special-product-price">`).text(special_price));
+	//$product.append($(`button class="btn-add-to-cart">`));
 	return $product;
 };
 
