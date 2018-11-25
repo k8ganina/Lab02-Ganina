aca1cd0102dd51e699175f1450b0deb5d25c56c7
Katerina <ganina.katerina2915@gmail.com>
2018-11-25 15:41:17 +0200

    Bootstrap and jQuery install

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 console.log('Hello!');
 console.log(`The time is ${new Date()}`);
 import './scss/main.scss';
+
+
+import $ from 'jquery';
+window.jQuery = $;
+window.$ = $;
+
+
+let _makeProduct = require('./modules/product-html');
